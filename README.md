2000453097b2ca82b5b74a4b1c316ff0c2b830ae
Adam Lith <adam.lith@gmail.com>
2017-07-31 09:48:55 +0200

    Creates Solution, project, enricher and documentation.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # serilog-enrichers-author
+
+Includes the serilog enricher AuthorEnricher that will add an "Author" object to each log message.
+The author object contains four properties:
+* Team (e.g. "Heimdal" or "Ace of Space")
+* Department (e.g. "Payments" or "Solutions")
+* _(Optional)_ ServiceGroup (If this service is part of a larger group of services, then the name of that group, e.g. "Edge" or "Loans")
+* _(Optional)_ Repository (The url to where the source code can be found)
+ 
+
+Use it like this:
+```csharp
+var logger = new LoggerConfiguration()
+                 .Enrich.With(new AuthorEnricher(
+                                    teamName: "Heimdal", 
+                                    department: "Payments", 
+                                    serviceGroup: "Edge",
+                                    repositoryUrl: new Uri("https://github.com/collector-bank/serilog-enrichers-author")))
+                 .CreateLogger();
+```
---
+++ src/Collector.Serilog.Enrichers.Author/AuthorEnricher.cs
@@ -0,0 +1,38 @@
+﻿namespace Collector.Serilog.Enrichers.Author
+{
+    using System;
+    using System.Collections.Generic;
+
+    using global::Serilog.Core;
+    using global::Serilog.Events;
+
+    public class AuthorEnricher : ILogEventEnricher
+    {
+        private readonly IDictionary<string, string> _dictionary;
+        
+        public AuthorEnricher(string teamName, string department, Uri repositoryUrl = null, string serviceGroup = null)
+        {
+            if (teamName == null)
+                throw new ArgumentNullException(nameof(teamName));
+            if (department == null)
+                throw new ArgumentNullException(nameof(department));
+
+            _dictionary = new Dictionary<string, string>
+                          {
+                              ["Team"] = teamName,
+                              ["Department"] = department
+                          };
+
+            if (repositoryUrl != null)
+                _dictionary["Repository"] = repositoryUrl.ToString();
+
+            if (serviceGroup != null)
+                _dictionary["ServiceGroup"] = serviceGroup;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Author", _dictionary, destructureObjects: true));
+        }
+    }
+}
